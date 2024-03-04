33d5bb4f24a2c76913f132a832d718fe36641712
Austin <itzsaveyt@gmail.com>
2024-03-04 09:46:08 -0600

    Fixed an issue with the item whitelist in the PLAYER_CONSUME objective.

--- src/main/java/net/zithium/tournaments/objective/internal/PlayerConsumeObjective.java
+++ src/main/java/net/zithium/tournaments/objective/internal/PlayerConsumeObjective.java
@@ -2,13 +2,17 @@ package net.zithium.tournaments.objective.internal;
 
 import net.zithium.tournaments.objective.XLObjective;
 import net.zithium.tournaments.tournament.Tournament;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
 
 public class PlayerConsumeObjective extends XLObjective {
     public PlayerConsumeObjective() {
@@ -18,7 +22,8 @@ public class PlayerConsumeObjective extends XLObjective {
     @Override
     public boolean loadTournament(Tournament tournament, FileConfiguration config) {
         if (config.contains("item_whitelist")) {
-            tournament.setMeta("ITEM_WHITELIST_" + tournament.getIdentifier(), config.getStringList("item_whitelist"));
+            Set<String> itemWhitelist = new HashSet<>(config.getStringList("item_whitelist"));
+            tournament.setMeta("ITEM_WHITELIST_" + tournament.getIdentifier(), itemWhitelist);
         }
         return true;
     }
@@ -29,20 +34,25 @@ public class PlayerConsumeObjective extends XLObjective {
         Player player = event.getPlayer();
         ItemStack consumedItem = event.getItem();
 
+        Bukkit.getLogger().log(Level.INFO, "Consumed Item: " + consumedItem.getType());
         if (consumedItem == null || !consumedItem.getType().isEdible()) {
             return;
         }
 
         for (Tournament tournament : getTournaments()) {
+
+            Bukkit.getLogger().log(Level.WARNING, "Whitelist: " + tournament.getMeta("ITEM_WHITELIST_" + tournament.getIdentifier()));
+
+
             if (canExecute(tournament, player)) {
                 String tournamentIdentifier = tournament.getIdentifier();
 
                 if (tournament.hasMeta("ITEM_WHITELIST_" + tournamentIdentifier)) {
-                    List<String> whitelist = (List<String>) tournament.getMeta("ITEM_WHITELIST_" + tournamentIdentifier);
+                    @SuppressWarnings("unchecked")
+                    Set<String> whitelist = (Set<String>) tournament.getMeta("ITEM_WHITELIST_" + tournamentIdentifier);
+                    Material consumedMaterial = consumedItem.getType();
 
-                    String item = consumedItem.toString();
-
-                    if (whitelist.contains(item)) {
+                    if (whitelist.contains(consumedMaterial.toString())) {
                         tournament.addScore(player.getUniqueId(), 1);
                     }
                 } else {
