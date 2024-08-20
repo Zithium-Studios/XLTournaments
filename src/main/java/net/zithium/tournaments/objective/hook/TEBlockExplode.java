d07b1be56c0d2b2d6347ac0295f273ca33416ee6
Austin <itzsaveyt@gmail.com>
2024-08-20 10:26:16 -0500

    Update TokenEnchantAPI
    Updated the TokenEnchantAPI to the latest version as previous version was no longer working and package names have changed.

--- src/main/java/net/zithium/tournaments/objective/hook/TEBlockExplode.java
+++ src/main/java/net/zithium/tournaments/objective/hook/TEBlockExplode.java
@@ -1,8 +1,3 @@
-/*
- * XLTournaments Plugin
- * Copyright (c) 2020 - 2022 Lewis D (ItsLewizzz). All rights reserved.
- */
-
 package net.zithium.tournaments.objective.hook;
 
 import com.vk2gpz.tokenenchant.event.TEBlockExplodeEvent;
